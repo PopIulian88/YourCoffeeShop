074adece40501e94487542be057122407372d4ef
Pop Iulian <iulian.pop03@e-uvt.ro>
2023-09-07 13:33:15 +0300

    Admin tab bar finish

--- myCoffeeShop/src/main/resources/static/Screens/AdminScreens/Admin.js
+++ myCoffeeShop/src/main/resources/static/Screens/AdminScreens/Admin.js
@@ -2,15 +2,16 @@ import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import Menu from "./Menu";
 import Stock from "./Stock";
 import Profile from "./Profile";
+import {admin_styles} from "../../Style/Admin_style/Admin_styles";
 
 const Tab = createBottomTabNavigator();
 
 export default function Admin() {
     return (
-        <Tab.Navigator initialRouteName={"Stock"}>
-            <Tab.Screen options={{headerShown:false}} name="Menu" component={Menu}/>
-            <Tab.Screen options={{headerShown:false}} name={"Stock"} component={Stock}/>
-            <Tab.Screen options={{headerShown:false}} name={"Profile"} component={Profile}/>
+        <Tab.Navigator initialRouteName={"Stock"} screenOptions={admin_styles.screenOptions}>
+            <Tab.Screen options={admin_styles.menuOptions} name="Menu" component={Menu}/>
+            <Tab.Screen options={admin_styles.stockOptions} name={"Stock"} component={Stock}/>
+            <Tab.Screen options={admin_styles.profileOptions} name={"Profile"} component={Profile}/>
         </Tab.Navigator>
     );
 
---
+++ myCoffeeShop/src/main/resources/static/Style/Admin_style/Admin_styles.js
@@ -0,0 +1,47 @@
+import {StyleSheet} from "react-native";
+import {BACKGROUND_COLOR, DARK_GREEN, MY_GRAY} from "../../Colors";
+import {FontAwesome5, MaterialIcons} from "@expo/vector-icons";
+
+export const admin_styles = StyleSheet.create({
+    screenOptions: {
+        tabBarStyle:{
+            backgroundColor: BACKGROUND_COLOR,
+        },
+
+        // tabBarItemStyle:{
+        //     backgroundColor:'#00ff00',
+        //     margin:5,
+        //     borderRadius:10,
+        // }
+
+        tabBarActiveTintColor: DARK_GREEN,
+        activeTintColor: DARK_GREEN,
+
+        tabBarInactiveTintColor: MY_GRAY,
+    },
+
+    menuOptions: {
+        headerShown:false,
+
+        tabBarIcon: ({focused}) => (
+            <MaterialIcons name='menu-book' size={25} color={focused ? DARK_GREEN : MY_GRAY} />
+        ),
+    },
+
+    stockOptions: {
+        headerShown:false,
+
+        tabBarIcon: ({focused}) => (
+            <FontAwesome5 name='boxes' size={25} color={focused ? DARK_GREEN : MY_GRAY}/>
+        ),
+    },
+
+    profileOptions: {
+        headerShown:false,
+
+        tabBarIcon: ({focused}) => (
+            <FontAwesome5 name='house-user' size={25} color={focused ? DARK_GREEN : MY_GRAY}/>
+        ),
+    },
+
+});
